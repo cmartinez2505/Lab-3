48a159545242289d3570561042a9c9a814e4c237
Chris <chrismtz2003@gmail.com>
2026-06-30 15:30:07 -0400

    Displayed my header on top of the code

---
+++ Lab10_cmartinez42-1.py
@@ -0,0 +1,7 @@
+"""
+Program Name: Word Analyzer
+Author: Chris Martinez
+Purpose: This is a OOP Program that is used to count word frequencies in the text files.
+Starter Code: No starter code used
+Date: June 30, 2026
+"""
